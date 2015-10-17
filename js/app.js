ad1b0b4384e6564e2604889bcf6c2f49fdadcc7c
Eric Chen <ericc7991@gmail.com>
2015-10-17 01:44:04 -0400

    support page

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	//parallax js
+        var jumboHeight = $('.jumbotron').outerHeight();
+        function parallax(){
+            var scrolled = $(window).scrollTop();
+            $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+        }
+        $(window).scroll(function(e){
+            parallax();
+        });
+
+    //sponsor js 
+    	$("#sponsor").mouseenter(function () {
+    		$(this).fadeTo('fast', 0.8).mouseleave(function() {
+    			$(this).fadeTo('fast', 1.0);	
+    		});
+    	});
+});
+        
